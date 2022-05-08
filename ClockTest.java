ae1728bdb2d0ea765a57543eaffed0549e9f9464
Ayanwoye Charles Damilola <c.ayanwoye0@gmail.com>
2022-05-08 12:03:54 -0700

    Delete ClockTest.java

--- ClockTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ClockTest {
-	public static void main(String[] args) {
-		Clock clock1 = new Clock(22, 45, 50);
-		
-		System.out.printf("The time is %s%n:", clock1.getDisplayTime());
-	}
-}
